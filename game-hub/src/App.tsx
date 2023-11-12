3a2dc1fd5c41f3fbd309caf3831bb34118d0b058
Sudharsan <sudharsan.ravichandran@whatsplan.app>
2023-11-12 10:07:30 +0530

    Build a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <Button colorScheme="blue">Button</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //larger than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        NavBar
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="tomato">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
